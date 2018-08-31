38d9e815ff1aacbddffeb74db7393d6b58408e80
Roman Ionut Goga <roman-ionut.goga@accenture.com>
2018-08-31 12:38:42 +0300

    Initial commit

---
+++ src/main/java/com/cfm/pwlc/controllers/SampleController.java
@@ -0,0 +1,14 @@
+package com.cfm.pwlc.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SampleController implements Initializable {
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/com/cfm/pwlc/service/ListCreatorService.java
@@ -0,0 +1,7 @@
+package com.cfm.pwlc.service;
+
+/**
+ * Created by roman-ionut.goga on 8/31/2018.
+ */
+public class ListCreatorService {
+}
